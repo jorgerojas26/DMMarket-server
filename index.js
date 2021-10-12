2e11daeae9f65f2ab70fbf22fe4a9245f24b4ca5
Jorge Rojas <jorgeluisrojasb@gmail.com>
2021-10-12 15:48:56 -0400

    adding buy price fluctuation report

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const MONTHS = {
+  Enero: 'Enero',
+  Febrero: 'Febrero',
+  Marzo: 'Marzo',
+  Abril: 'Abril',
+  Mayo: 'Mayo',
+  Junio: 'Junio',
+  Agosto: 'Agosto',
+  Septiembre: 'Septiembre',
+  Octubre: 'Octubre',
+  Noviembre: 'Noviembre',
+  Diciembre: 'Diciembre',
+};
+
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
@@ -67,6 +81,78 @@ app.use('/api/reports/invoice', async (req, res) => {
   };
 
   res.status(200).json(response);
+});
+
+app.use('/api/reports/products/buy-price-fluctuation/:productId', async (req, res) => {
+  const { productId } = req.params;
+
+  let response = await knex
+    .select(
+      knex.raw(`
+          slavecomp.Descripcion,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 1, slavecomp.Precio, NULL)), 2)  AS Enero,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 1, slavecomp.idFactura, NULL))  AS Enero_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 2, slavecomp.Precio, NULL)), 2)  AS Febrero,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 2, slavecomp.idFactura, NULL))  AS Febrero_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 3, slavecomp.Precio, NULL)), 2)  AS Marzo,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 3, slavecomp.idFactura, NULL))  AS Marzo_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 4, slavecomp.Precio, NULL)), 2)  AS Abril,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 4, slavecomp.idFactura, NULL))  AS Abril_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 5, slavecomp.Precio, NULL)), 2)  AS Mayo,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 5, slavecomp.idFactura, NULL))  AS Mayo_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 6, slavecomp.Precio, NULL)), 2)  AS Junio,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 6, slavecomp.idFactura, NULL))  AS Junio_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 7, slavecomp.Precio, NULL)), 2)  AS Julio,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 7, slavecomp.idFactura, NULL))  AS Julio_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 8, slavecomp.Precio, NULL)), 2)  AS Agosto,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 8, slavecomp.idFactura, NULL))  AS Agosto_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 9, slavecomp.Precio, NULL)), 2) AS Septiembre,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 9, slavecomp.idFactura, NULL))  AS Septiembre_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 10, slavecomp.Precio, NULL)), 2) AS Octubre,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 10, slavecomp.idFactura, NULL))  AS Octubre_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 11, slavecomp.Precio, NULL)), 2) AS Noviembre,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 11, slavecomp.idFactura, NULL))  AS Noviembre_transactions,
+       ROUND(AVG(IF(MONTH(mastercomp.Fecha) = 12, slavecomp.Precio, NULL)), 2) AS Diciembre,
+       COUNT(IF(MONTH(mastercomp.Fecha) = 12, slavecomp.idFactura, NULL))  AS Diciembre_transactions
+            `)
+    )
+    .from('slavecomp')
+    .innerJoin('mastercomp', function () {
+      this.on('mastercomp.idFactura', 'slavecomp.idFactura').andOn('mastercomp.Anulada', 0);
+    })
+    .where(knex.raw('YEAR(mastercomp.Fecha)'), knex.raw('YEAR(CURDATE())'))
+    .andWhere('slavecomp.idProducto', productId.toString());
+
+  response = response.reduce(
+    (acc, current) => ({
+      id: current.Descripcion,
+      data: Object.keys(MONTHS).map((month) => ({ x: month, y: current[month] }), []),
+    }),
+    {}
+  );
+  res.status(200).json(response);
+});
+
+app.use('/api/products', async (req, res) => {
+  const { filter } = req.query;
+  if (filter) {
+    try {
+      const response = await knex
+        .select('idProducto', 'Descripcion')
+        .from('productos')
+        .where(knex.raw(`Descripcion LIKE '%${filter}%'`));
+      res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    try {
+      const response = await knex.select('idProducto', 'Descripcion').from('productos');
+      res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
 
 app.use(express.static(path.join(__dirname, 'client/build')));
